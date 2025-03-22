6399aba91608b9258531dd9e84a8ec65c1ac7fbb
PuneetGOTO <an920513@gmail.com>
2025-03-22 23:30:41 +0800

    将Discord重定向URI从localhost改为127.0.0.1，解决连接问题

--- app/discord/config.py
+++ app/discord/config.py
@@ -4,7 +4,7 @@ import os
 # Discord API设置
 DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_ID', '')
 DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET', '')
-DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI', 'http://localhost:8080/discord/callback')
+DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI', 'http://127.0.0.1:8080/discord/callback')
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN', '')
 
 # OAuth授权范围
